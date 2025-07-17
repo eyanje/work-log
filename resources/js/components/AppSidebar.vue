98993850588efadeb3bda01ca8224149a36ce855
Edward Yan <eyanje@gmail.com>
2025-07-16 21:19:30 -0400

    Add branding for "Journal"

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -33,16 +33,6 @@ const mainNavItems: NavItem[] = computed(() => [
 ]);
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
 ];
 </script>
 
