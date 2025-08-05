9dfbd4a461395e839832064f364eae8fbe7667f7
Edward Yan <eyanje@gmail.com>
2025-08-05 16:38:15 -0400

    Build builder, backend, and frontend images

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
-docker build -t work-log-builder -f ./docker/builder/Dockerfile . \
-	&& docker compose -f compose.prod.yaml build
+docker build -t work-log-builder -f ./docker/builder/Dockerfile .
+docker build -t work-log-web -f ./docker/nginx/Dockerfile .
+docker build -t work-log-php-fpm -f ./docker/php-fpm/Dockerfile .
 
