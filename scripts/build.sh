6949fcf88e39186d31f5d93de961918fb62833ab
Edward Yan <eyanje@gmail.com>
2025-08-29 17:23:32 -0400

    Use host network to run buildah

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,26 +7,31 @@ REPOSITORY=harbor.eyanje.net/work-log
 TAG=$(git rev-parse HEAD)
 
 docker build \
+    --network host \
 	-t $REPOSITORY/build-agent:$TAG \
 	-t $REPOSITORY/build-agent:latest \
 	-f ./docker/build-agent/Dockerfile .
 docker build \
+    --network host \
 	-t $REPOSITORY/base:$TAG \
 	-t $REPOSITORY/base:latest \
 	-f ./docker/base/Dockerfile .
 docker build \
+    --network host \
 	--build-arg REPOSITORY=$REPOSITORY \
 	--build-arg BASE_TAG=$TAG \
 	-t $REPOSITORY/migration:$TAG \
 	-t $REPOSITORY/migration:latest \
 	-f ./docker/migration/Dockerfile .
 docker build \
+    --network host \
 	--build-arg REPOSITORY=$REPOSITORY \
 	--build-arg BASE_TAG=$TAG \
 	-t $REPOSITORY/web:$TAG \
 	-t $REPOSITORY/web:latest \
 	-f ./docker/web/Dockerfile .
 docker build \
+    --network host \
 	--build-arg REPOSITORY=$REPOSITORY \
 	--build-arg BASE_TAG=$TAG \
 	-t $REPOSITORY/php-fpm:$TAG \
