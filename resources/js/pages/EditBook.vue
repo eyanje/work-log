62ce403718adca0a36ee8fc114ae2fb13f49608c
Edward Yan <eyanje@gmail.com>
2025-09-03 00:38:55 -0400

    Align forms to a grid

--- resources/js/pages/EditBook.vue
+++ resources/js/pages/EditBook.vue
@@ -13,11 +13,10 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { Book } from '@/types/book.d.ts';
+import type { Book } from '@/types/book.d.ts';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
 const { book } = defineProps<{
-    status?: string;
     book: Book;
 }>();
 
@@ -53,85 +52,98 @@ const submitImport = () => {
     <Head :title="`${book.title} | Edit`" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
-            {{ status }}
-        </div>
-
-        <div class="m-4">
+        <div class="m-4 w-xl pb-4">
             <h1>Edit {{ book.title }}</h1>
 
-            <h2>Metadata</h2>
-            <form @submit.prevent="submit" class="mr-auto w-fit">
-                <p class="grid gap-2">
-                    <Label for="title">Title</Label>
-                    <Input v-model="form.title" name="title" placeholder="My Book" />
-                </p>
-                <p>
-                    <Button type="submit" :disabled="!form.isDirty" class="col-span-full">Save</Button>
-                </p>
-            </form>
+            <section>
+                <h2>Metadata</h2>
+                <form @submit.prevent="submit">
+                    <div class="input-group">
+                        <Label for="title">Title</Label>
+                        <Input v-model="form.title" name="title" placeholder="My Book" />
+                    </div>
+                    <Button type="submit" :disabled="!form.isDirty">Save</Button>
+                </form>
+            </section>
 
-            <h2>Import</h2>
-            <p>Update books with iCalendar journal data.</p>
-            <form class="mr-auto w-fit gap-2" @submit.prevent="submitImport">
-                <p class="grid gap-2">
-                    <Label for="book">iCalendar data</Label>
-                    <Input type="file" accept="text/calendar" name="book"
-                    @input="importForm.book = $event.target.files[0]" required />
-                </p>
-                <p>
+            <section>
+                <h2>Import</h2>
+                <p>Update books with iCalendar journal data.</p>
+                <form class="gap-2" @submit.prevent="submitImport">
+                    <div class="input-group">
+                        <Label for="book">iCalendar data</Label>
+                        <Input type="file" accept="text/calendar" name="book" @input="importForm.book = $event.target.files[0]" required />
+                    </div>
                     <Button type="submit">Import</Button>
-                </p>
-            </form>
+                </form>
+            </section>
 
-            <h2>Export</h2>
-            <p>Export notebooks regularly to avoid losing data.</p>
-            <p>
+            <section>
+                <h2>Export</h2>
+                <p>Export notebooks regularly to avoid losing data.</p>
                 <Button as-child>
                     <a :href="route('book.export', { id: book.id })">Export as iCalendar</a>
                 </Button>
-            </p>
+            </section>
 
-            <h2>Special Actions</h2>
-            <Dialog>
-                <DialogTrigger>
-                    <Button variant="destructive">Delete Book</Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader class="space-y-3">
-                        <DialogTitle>Are you sure you want to delete book "{{ book.title }}"?</DialogTitle>
-                        <DialogDescription> All of its data will be permanently deleted. </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter>
-                        <DialogClose>
-                            <Button type="button" variant="secondary">Cancel</Button>
-                        </DialogClose>
-                        <Button as-child variant="destructive">
-                            <Link :href="route('book.delete', { id: book.id })" method="delete">Delete</Link>
-                        </Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
+            <section>
+                <h2>Special Actions</h2>
+                <Dialog>
+                    <DialogTrigger>
+                        <Button variant="destructive" class="w-full">Delete Book</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader class="space-y-3">
+                            <DialogTitle>Are you sure you want to delete book "{{ book.title }}"?</DialogTitle>
+                            <DialogDescription> All of its data will be permanently deleted. </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <DialogClose>
+                                <Button type="button" variant="secondary">Cancel</Button>
+                            </DialogClose>
+                            <Button as-child variant="destructive">
+                                <Link :href="route('book.delete', { id: book.id })" method="delete">Delete</Link>
+                            </Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            </section>
         </div>
     </AppLayout>
 </template>
 
 <style>
 h1 {
-    margin-bottom: calc(var(--spacing) * 3);
+    margin-bottom: calc(var(--spacing) * 6);
+    font-size: xx-large;
+    font-weight: bold;
+}
+
+section {
+    margin-top: calc(var(--spacing) * 9);
+    margin-bottom: calc(var(--spacing) * 9);
+}
+
+h2 {
+    margin-top: calc(var(--spacing) * 4);
+    margin-bottom: calc(var(--spacing) * 4);
     font-size: x-large;
     font-weight: bold;
 }
 
-h2 {
-    margin-top: calc(var(--spacing) * 6);
-    margin-bottom: calc(var(--spacing) * 1);
-    font-size: large;
-    font-weight: bold;
+p,
+.input-group {
+    margin-top: calc(var(--spacing) * 4);
+    margin-bottom: calc(var(--spacing) * 4);
 }
 
-form,
-p {
-    margin-bottom: calc(var(--spacing) * 1);
+.input-group {
+    display: grid;
+    gap: calc(var(--spacing) * 2);
+}
+
+form button {
+    display: block;
+    width: 100%;
 }
 </style>
